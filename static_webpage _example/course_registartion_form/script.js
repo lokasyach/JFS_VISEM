cde2fea8755a16a28183250e42fd5de2f236c0a4
lokasya-chandra <lokasyachandra2025@gmail.com>
2025-12-02 11:37:50 +0530

    done

---
+++ static_webpage _example/course_registartion_form/script.js
@@ -0,0 +1,57 @@
+
+let subjects = document.querySelectorAll(".subject");
+let totalBox = document.getElementById("total");
+let resultBox = document.getElementById("resultBox"); // 🔹 NEW
+
+subjects.forEach(item => {
+    item.addEventListener("change", () => {
+        let total = 0;
+
+        subjects.forEach(sub => {
+            if (sub.checked) {
+                total += parseInt(sub.value);
+            }
+        });
+
+        totalBox.innerText = "₹" + total;
+    });
+});
+
+document.getElementById("regForm").addEventListener("submit", function(e){
+    e.preventDefault();
+      e.preventDefault();
+
+    let selectedSubjects = [];
+    let totalFee = 0;
+
+    subjects.forEach(sub => {
+        if (sub.checked) {
+
+            let subjectName = sub.parentElement.innerText.trim();
+            selectedSubjects.push(subjectName);
+
+            totalFee += parseInt(sub.value);
+        }
+    });
+
+    if (selectedSubjects.length === 0) {
+     
+        resultBox.style.display = "block";
+        resultBox.className = "result-box error";
+        resultBox.innerHTML = "<b>Error:</b> Please select at least one subject.";
+        return;
+    }
+
+    let studentName = document.getElementById("name").value;
+
+    let message =
+        "Student Name: " + studentName + "\n\n" +
+        "Selected Subjects:\n- " + selectedSubjects.join("\n- ") + "\n\n" +
+        "Total Fee: ₹" + totalFee;
+
+   
+    resultBox.style.display = "block";
+    resultBox.className = "result-box success";
+    
+    resultBox.innerHTML = message.replace(/\n/g, "<br>");
+});
